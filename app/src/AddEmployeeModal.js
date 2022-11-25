74301972c50fae7b8ad1542497b29813290f4163
Joshua Newton <jonewton0905@gmail.com>
2022-11-25 18:29:24 -0500

    Started building UI for adding employee, very much WIP

--- app/src/AddEmployeeModal.js
+++ app/src/AddEmployeeModal.js
@@ -8,7 +8,58 @@ const AddEmployeeModal = (props) => {
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        ...
+                        <div className="row">
+                            <div className="col-3">
+                                <label>ID</label>
+                            </div>
+                            <div className="col-9">
+                                <input id="idInput" type={"text"}></input>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <label>LastName</label>
+                            </div>
+                            <div className="col-9">
+                                <input id="idLastName" type={"text"}></input>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <label>FirstName</label>
+                            </div>
+                            <div className="col-9">
+                                <input id="idFirstName" type={"text"}></input>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <label>DOB</label>
+                            </div>
+                            <div className="col-9">
+                                <input id="idDOB" type={"date"}></input>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <label>Salary</label>
+                            </div>
+                            <div className="col-9">
+                                <input id="idSalary" type={"number"}></input>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <label>Dependents</label>
+                            </div>
+                            <div className="col-9">
+                                <input id="idDependents" list="dependentsList"></input>
+                                <datalist id="dependentsList">
+                                    <option value="dependent1"></option>
+                                    <option value="dependent2"></option>
+                                </datalist>
+                            </div>
+                        </div>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
