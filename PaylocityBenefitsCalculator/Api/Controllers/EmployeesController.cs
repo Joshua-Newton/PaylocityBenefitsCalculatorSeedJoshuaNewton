082100e29b7ba8e17758a981c636baf527ef6c09
Joshua Newton <jonewton0905@gmail.com>
2022-11-27 16:02:20 -0500

    Changed GetAll to read the json file instead of hard coded data; changed default state of json file to match proper format.

--- PaylocityBenefitsCalculator/Api/Controllers/EmployeesController.cs
+++ PaylocityBenefitsCalculator/Api/Controllers/EmployeesController.cs
@@ -3,13 +3,18 @@ using Api.Dtos.Employee;
 using Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Api.Controllers
 {
+
     [ApiController]
     [Route("api/v1/Employees")]
     public class EmployeesController : ControllerBase
     {
+        private string jsonFilePath = "EmployeeData.json";
+
         [SwaggerOperation(Summary = "Get employee by id")]
         [HttpGet("{id}")]
         public async Task<ActionResult<ApiResponse<GetEmployeeDto>>> Get(int id)
@@ -21,80 +26,44 @@ namespace Api.Controllers
         [HttpGet("")]
         public async Task<ActionResult<ApiResponse<List<GetEmployeeDto>>>> GetAll()
         {
-            //task: use a more realistic production approach
-            var employees = new List<GetEmployeeDto>
+            var employees = new List<GetEmployeeDto>();
+
+            using (StreamReader r = new StreamReader(jsonFilePath))
             {
-                new()
+                string json = r.ReadToEnd();
+                employees = JsonSerializer.Deserialize<List<GetEmployeeDto>>(json);
+            }
+
+            if (employees != null && employees.Count > 0)
+            {
+                if(employees.Count > 0)
                 {
-                    Id = 1,
-                    FirstName = "LeBron",
-                    LastName = "James",
-                    Salary = 75420.99m,
-                    DateOfBirth = new DateTime(1984, 12, 30)
-                },
-                new()
-                {
-                    Id = 2,
-                    FirstName = "Ja",
-                    LastName = "Morant",
-                    Salary = 92365.22m,
-                    DateOfBirth = new DateTime(1999, 8, 10),
-                    Dependents = new List<GetDependentDto>
+                    var result = new ApiResponse<List<GetEmployeeDto>>
                     {
-                        new()
-                        {
-                            Id = 1,
-                            FirstName = "Spouse",
-                            LastName = "Morant",
-                            Relationship = Relationship.Spouse,
-                            DateOfBirth = new DateTime(1998, 3, 3)
-                        },
-                        new()
-                        {
-                            Id = 2,
-                            FirstName = "Child1",
-                            LastName = "Morant",
-                            Relationship = Relationship.Child,
-                            DateOfBirth = new DateTime(2020, 6, 23)
-                        },
-                        new()
-                        {
-                            Id = 3,
-                            FirstName = "Child2",
-                            LastName = "Morant",
-                            Relationship = Relationship.Child,
-                            DateOfBirth = new DateTime(2021, 5, 18)
-                        }
-                    }
-                },
-                new()
-                {
-                    Id = 3,
-                    FirstName = "Michael",
-                    LastName = "Jordan",
-                    Salary = 143211.12m,
-                    DateOfBirth = new DateTime(1963, 2, 17),
-                    Dependents = new List<GetDependentDto>
-                    {
-                        new()
-                        {
-                            Id = 4,
-                            FirstName = "DP",
-                            LastName = "Jordan",
-                            Relationship = Relationship.DomesticPartner,
-                            DateOfBirth = new DateTime(1974, 1, 2)
-                        }
-                    }
+                        Data = employees,
+                        Success = true
+                    };
+                    return result;
                 }
-            };
-            
-            var result = new ApiResponse<List<GetEmployeeDto>>
+                else
+                {
+                    var result = new ApiResponse<List<GetEmployeeDto>>()
+                    {
+                        Success = true,
+                        Message = "No Employees Exist"
+                    };
+                    return result;
+                }
+            }
+            else
             {
-                Data = employees,
-                Success = true
-            };
-            
-            return result;
+                var result = new ApiResponse<List<GetEmployeeDto>>()
+                {
+                    Success = false,
+                    Message = "Employee JSON Parse failed"
+                };
+                return result;
+            }
         }
 
         [SwaggerOperation(Summary = "Add employee")]
